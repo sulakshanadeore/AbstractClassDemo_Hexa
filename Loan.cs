678c1114988953d38fa491aef7fef2bea44be05a
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-03-31 12:17:12 +0530

    Add files via upload

---
+++ Loan.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsContinuedAbstractDemo
+{
+   public class Loan
+    {
+
+        public int LoanID { get; set; }
+        public string loanType { get; set; }
+
+        public double loanAmt { get; set; }
+
+        public int Tenure { get; set; }
+
+
+
+
+    }
+
+   public class HomeLoan : Loan
+    {
+        public float ROI { get; set; }
+        public virtual double calculateEMI(double loanAmt,int Tenure, float ROI)
+        {
+            double emi=0;
+            return emi;
+        }
+
+    }
+
+  public  class PersonalLoan : HomeLoan
+    {
+        public override double calculateEMI(double loanAmt, int Tenure, float ROI)
+        {
+            double emi=0;
+            return emi;
+        }
+
+
+    }
+
+}
